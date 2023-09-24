256fd0a80511b5c5fb1d7f5376112fdba89e9306
mariananghel05 <mariananghel99@gmail.com>
2023-09-24 20:53:24 +0300

    auto

--- StartMonitoring.ps1
+++ StartMonitoring.ps1
@@ -1,25 +1,9 @@
-### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
-    $watcher = New-Object System.IO.FileSystemWatcher
-    $watcher.Path = "C:\Users\Nikolas\Desktop\books\"
-    $watcher.Filter = "*.*"
-    $watcher.IncludeSubdirectories = $true
-    $watcher.EnableRaisingEvents = $true  
-
-### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
-    $action = { $path = $Event.SourceEventArgs.FullPath
-                $changeType = $Event.SourceEventArgs.ChangeType
-                $logline = "$(Get-Date), $changeType, $path"
-                Add-content "C:\Users\Nikolas\Desktop\books\log.txt" -value $logline
-                $add = git add .
-                $commit = git commit -m "auto"
-                $push = git push -u books master
-                Add-content $add -value $logline
-                Add-content $commit -value $logline
-                Add-content $push -value $logline
-              }    
-### DECIDE WHICH EVENTS SHOULD BE WATCHED 
-    Register-ObjectEvent $watcher "Created" -Action $action
-    Register-ObjectEvent $watcher "Changed" -Action $action
-    Register-ObjectEvent $watcher "Deleted" -Action $action
-    Register-ObjectEvent $watcher "Renamed" -Action $action
-    while ($true) {sleep 5}
+echo "Preparing ..."
+echo "Adding files to repo ..."
+$add = git add .
+echo "commiting files ..."
+$commit = git commit -m "auto"
+echo "Pushing changes  ..."
+$push = git push -u books master
+echo "Done."
+echo "If there's errors You should check if remote repo is added and variables[git.name, git.email] are configurated."
