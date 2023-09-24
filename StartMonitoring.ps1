99ab50d3f47a0eee8ab06a6b084e34cc52d72ff1
mariananghel05 <mariananghel99@gmail.com>
2023-09-24 20:36:32 +0300

    auto

---
+++ StartMonitoring.ps1
@@ -0,0 +1,25 @@
+### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
+    $watcher = New-Object System.IO.FileSystemWatcher
+    $watcher.Path = "C:\Users\Nikolas\Desktop\books\"
+    $watcher.Filter = "*.*"
+    $watcher.IncludeSubdirectories = $true
+    $watcher.EnableRaisingEvents = $true  
+
+### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
+    $action = { $path = $Event.SourceEventArgs.FullPath
+                $changeType = $Event.SourceEventArgs.ChangeType
+                $logline = "$(Get-Date), $changeType, $path"
+                Add-content "C:\Users\Nikolas\Desktop\books\log.txt" -value $logline
+                $add = git add .
+                $commit = git commit -m "auto"
+                $push = git push -u origin master
+                echo $add
+                echo $commit
+                echo $push
+              }    
+### DECIDE WHICH EVENTS SHOULD BE WATCHED 
+    Register-ObjectEvent $watcher "Created" -Action $action
+    Register-ObjectEvent $watcher "Changed" -Action $action
+    Register-ObjectEvent $watcher "Deleted" -Action $action
+    Register-ObjectEvent $watcher "Renamed" -Action $action
+    while ($true) {sleep 5}
